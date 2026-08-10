6ee4b2d28120e3cd63ef3085aa9b7ba4a2a90072
Harikrishnan <118582559+2022-DEV3-086@users.noreply.github.com>
2026-08-10 18:29:01 +0530

    feat: add spring security filter chain to permit user registration endpoint

--- src/main/java/com/harikrishnan/finflow/configuration/AppConfiguration.java
+++ src/main/java/com/harikrishnan/finflow/configuration/AppConfiguration.java
@@ -1,9 +1,32 @@
 package com.harikrishnan.finflow.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableJpaAuditing
 public class AppConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) {
+        httpSecurity.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(a -> a.requestMatchers("/user/register").permitAll().anyRequest().authenticated());
+
+        return httpSecurity.build();
+    }
+
+
 }
